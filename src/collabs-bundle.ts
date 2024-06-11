7e0ddb6f51e189ec386e45dae5f9737ef0588b96
Andreas Rozek <A.Rozek@gmx.de>
2024-06-11 16:01:20 +0200

    Update collabs-bundle.ts

--- src/collabs-bundle.ts
+++ src/collabs-bundle.ts
@@ -1,14 +1,20 @@
-import * as collabs             from "@collabs/collabs";
-import { IndexedDBDocStore }    from "@collabs/indexeddb";
-import { LocalStorageDocStore } from "@collabs/local-storage";
-import { TabSyncNetwork }       from "@collabs/tab-sync";
-import { WebSocketNetwork }     from "@collabs/ws-client";
+import * as collabs             from "@collabs/collabs"
+import { IndexedDBDocStore }    from "@collabs/indexeddb"
+import { LocalStorageDocStore } from "@collabs/local-storage"
+import { TabSyncNetwork }       from "@collabs/tab-sync"
+import { WebSocketNetwork }     from "@collabs/ws-client"
 
 const Collabs = {
   collabs, 
   IndexedDBDocStore, LocalStorageDocStore,
   TabSyncNetwork, WebSocketNetwork,
 }
+
+export *                        from "@collabs/collabs"
+export { IndexedDBDocStore }    from "@collabs/indexeddb"
+export { LocalStorageDocStore } from "@collabs/local-storage"
+export { TabSyncNetwork }       from "@collabs/tab-sync"
+export { WebSocketNetwork }     from "@collabs/ws-client"
 
 // @ts-ignore allow "window.collabs"
 window.collabs = Collabs
